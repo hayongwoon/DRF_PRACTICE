9a55fe59b9c3fff6376623ceb365a6ef39bc7c9c
hayongwoon <a01046641857@gmail.com>
2022-06-08 20:27:57 +0900

    setting drf

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from email import message
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+
+def index(request):
+
+    return JsonResponse({'message': 'success'})
