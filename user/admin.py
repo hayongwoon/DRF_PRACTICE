799a1bc70cae26de95ce91dd1e8e67cd8e2070e0
hayongwoon <a01046641857@gmail.com>
2022-06-09 20:18:50 +0900

    admin page deco

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, UserProfile, Hobby
 # Register your models here.
+
+class Useradmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username' )
+
+admin.site.register(User, Useradmin)
+admin.site.register(UserProfile)
+admin.site.register(Hobby)
--- user/models.py
+++ user/models.py
@@ -7,12 +7,20 @@ class User(models.Model):
     fullname = models.CharField("이름", max_length=20)
     join_date = models.DateTimeField("가입일", auto_now_add=True)
 
+    def __str__(self):
+        return self.username
+
+
 class UserProfile(models.Model):
-    user = models.ForeignKey(to=User, verbose_name="사용자", unique=True, on_delete=models.CASCADE)
-    hobby = models.ManyToManyField(to="Hobby", verbose_name="취미")
+    user = models.ForeignKey(to=User, verbose_name="사용자", on_delete=models.CASCADE, unique=True)
+    hobby = models.ManyToManyField(to='Hobby', verbose_name="취미") #to=문자열 은 하비 모델이 해당 선언 시 보다 아래에 있어서 ''를 붙여줘야한다.
     introduction = models.TextField("소개")
     birthday = models.DateField("생일")
     age = models.IntegerField("나이")
 
+
 class Hobby(models.Model):
     name = models.CharField("취미", max_length=50)
+
+    def __str__(self):
+        return self.name
