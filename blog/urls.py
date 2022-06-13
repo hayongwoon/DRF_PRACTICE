d422571cf4867a373156391201e622c231faf634
hayongwoon <a01046641857@gmail.com>
2022-06-13 17:43:41 +0900

    blog url test

--- blog/urls.py
+++ blog/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import BlogApiVeiw
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
-    path('user/', include('user.urls')),
-    path('blog/', include('blog.urls')),
+    path('', BlogApiVeiw.as_view()),
+
 ]
