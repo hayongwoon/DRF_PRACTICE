47754dd281d8fc85db5640baadb132dffc1ff62a
hayongwoon <a01046641857@gmail.com>
2022-06-13 17:07:05 +0900

    add category model, article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from operator import mod
+from unicodedata import category
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField('카테고리', max_length=80)
+    explain = models.CharField('설명', max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Article(models.Model):
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
+    title = models.CharField('제목', max_length=80)
+    #ManytoMany 모델은 다중 선택이 가능
+    category = models.ManyToManyField(Category, verbose_name='카테고리')
+    content = models.TextField('내용', max_length=500)
+
+    
