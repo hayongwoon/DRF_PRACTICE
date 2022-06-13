3b7d8930e0952db1054b5b9cd0d3015cf78cd915
hayongwoon <a01046641857@gmail.com>
2022-06-13 17:55:42 +0900

    blog view test

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,7 @@ class BlogApiVeiw(APIView):
         if user.is_authenticated:
             return Response({'message': 'get method!!'})
 
+    # 게시글은 계정 생성 후 3일 이상 지난 사용자만 생성할 수 있도록 권한을 설정
+    # 테스트 코드에서는 계정 생성 후 3분 이상 지난 사용자는 게시글을 작성할 수 있도록 설정
+    def post(self, request):
+        return Response({'message': 'post method!!'})
