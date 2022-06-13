a9805d1aabac92ae9fbbd64f1691920dc935567e
hayongwoon <a01046641857@gmail.com>
2022-06-13 17:54:08 +0900

    blog view test

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Article
+
 
 # Create your views here.
+class BlogApiVeiw(APIView):
+    #로그인 한 사용자 게시글 정보 보여주는 view
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            return Response({'message': 'get method!!'})
+
