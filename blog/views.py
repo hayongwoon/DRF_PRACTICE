d6b575dcf5dd5b7d72afdcdcb1d5d118ebf644d4
hayongwoon <a01046641857@gmail.com>
2022-06-15 18:43:23 +0900

    add article 생성 permission

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ class RegistedMoreThan3daysUser(BasePermission):
     message = '가입 후 1주일 이상 지난 사용자만 사용하실 수 있습니다.'
     
     def has_permission(self, request, view):
-        return bool(request.user and request.user.join_date < (timezone.now() - timedelta(days=3))
+        return bool(request.user and request.user.join_date < (timezone.now() - timedelta(days=3)))
 
 
 # Create your views here.
