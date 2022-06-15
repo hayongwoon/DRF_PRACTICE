2a7d48c21e3d5d2e4df2574adb17b4777671cecb
hayongwoon <a01046641857@gmail.com>
2022-06-15 22:44:36 +0900

    add comment model in Blog app

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,14 @@ class Article(models.Model):
 
     def __str__(self) -> str:
         return f'작성자 : {self.user}, 제목 : {self.title}'
+
+# <게시글, 작성자, 작성 시간, 내용>이 포함된 comment라는 테이블을 추가해주세요
+# 게시글과 작성자는 fk 필드로 생성해주셔야 해요
+class Comment(models.Model):
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, verbose_name="게시글", on_delete=models.CASCADE)
+    content = models.TextField('내용', max_length=500)
+    create_time = models.DateTimeField("작성 시간", auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'작성자 : {self.user}, 내용 : {self.content}'
