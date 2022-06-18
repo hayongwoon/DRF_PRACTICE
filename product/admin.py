b64b0ad6be8a9b607e66cce83d6839df3d4d923b
hayongwoon <a01046641857@gmail.com>
2022-06-18 13:36:11 +0900

    Event admin register

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from product.models import Event as EventModel
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'register_date')
+
+admin.site.register(EventModel, EventAdmin)
+
