cb17208fcc8a85800442498cd1a29243c1285a1d
hayongwoon <a01046641857@gmail.com>
2022-06-18 13:18:19 +0900

    Event post view 이벤트 생성

--- product/models.py
+++ product/models.py
@@ -1,15 +1,15 @@
-from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField("제목", max_length=50)
-    thumbnail = models.ImageField("썸네일", upload_to="product/thumbnail", height_field=None, width_field=None, max_length=None)
-    description = models.TextField("설명")
-    created = models.DateTimeField("등록일자", auto_now_add=True)
-    exposure_start_date = models.DateField("노출 시작 시간")
-    exposure_end_date = models.DateField("노출 종료 시간")
-    is_active = models.BooleanField("활성화 여부")
+    title = models.CharField('제목', max_length=100)
+    thumbnail = models.ImageField('섬네일', upload_to='product/thumbnail',  height_field=None, width_field=None, max_length=None)
+    content = models.CharField('설명', max_length=120)
+    register_date = models.DateTimeField("등록일", auto_now_add=True)
+    start_propose_date = models.DateTimeField("노출 시작일") 
+    end_propose_date = models.DateTimeField("노출 종료일") 
+    is_active = models.BooleanField('활성화 여부', default=True) 
 
     def __str__(self):
-        return f"{self.title} 이벤트입니다."
+        return f"{self.title}이벤트 입니다."
--- product/views.py
+++ product/views.py
@@ -3,10 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from product.serializers import EventSerializer
+
 # Create your views here.
 class ProductApiVeiw(APIView):
     def get(self, request):
         return Response({'mssage': 'get_success'})
 
+    #이벤트 생성
     def post(self, request):
-        return Response({'mssage': 'post_success'})
+        serialized_event_data = EventSerializer(data=request.data)
+        if serialized_event_data.is_valid():
+            serialized_event_data.save()
+            return Response({'mssage': 'post_success'})
+
+        return Response({'mssage': 'post_fail'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #이벤트 수정
+    def put(self, request):
+        return Response({'msg': 'put success'})
