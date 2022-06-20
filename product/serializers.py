e4c5841941070fcde3364192864bf7ad0bfb018c
hayongwoon <a01046641857@gmail.com>
2022-06-20 18:41:51 +0900

    is_active false 에러 해결 : 시리얼라이즈 필드 설정

--- product/serializers.py
+++ product/serializers.py
@@ -4,7 +4,9 @@ from product.models import Event as EventModel
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventModel
-        fields = ["title", "content", "start_propose_date", "end_propose_date"]
+        fields = ["title", "content", "start_propose_date", "end_propose_date", "thumbnail"]
+
+    
 
 
 
