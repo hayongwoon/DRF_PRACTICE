450bd7077462c86fea80fc5ef5f518303e578fdc
hayongwoon <a01046641857@gmail.com>
2022-06-20 22:53:58 +0900

    fix event admin

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from product.models import Event as EventModel
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'register_date')
+    list_display = ('title', 'content', 'register_date', 'thumbnail')
 
 admin.site.register(EventModel, EventAdmin)
 
